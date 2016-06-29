8656eb410e60d59a85c13e6c9e0a4739ef3ffc53
Phil Nachum <phil.nachum@airbnb.com>
2016-06-29 16:53:31 -0700

    seed listings, groups, and listingGroups

--- server/main.js
+++ server/main.js
@@ -1,7 +1,20 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { Listings } from '../imports/api/listings/listings';
+import { ListingGroups } from '../imports/api/listingGroups/listingGroups';
+import { Groups } from '../imports/api/groups/groups';
 import '../imports/startup/server';
 
 Meteor.startup(() => {
   // setup Groups
+  //
+  Listings.remove({});
+  Groups.remove({});
+  ListingGroups.remove({});
+
+  Listings.insert({ _id: '1', location: '' });
+  Listings.insert({ _id: '2', location: '' });
+  Groups.insert({ _id: '1' });
+  ListingGroups.insert({ groupId: '1', listingId: '1' });
+  ListingGroups.insert({ groupId: '1', listingId: '2' });
 });
