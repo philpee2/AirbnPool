b9d83a9ebdbb20dc6817e66729f73d933bdde9b1
Michael Lawlor <michael.lawlor@airbnb.com>
2016-06-29 15:54:51 -0700

    Add pretty stylez for chat messages based on who the author is

--- imports/ui/ChatMessage.jsx
+++ imports/ui/ChatMessage.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { StyleSheet, css } from 'aphrodite';
+import TimeAgo from 'react-timeago';
 
 const propTypes = {
   message: PropTypes.object.isRequired,
@@ -7,12 +8,30 @@ const propTypes = {
 };
 
 export default function ChatMessage({ message, isSelf }) {
+  const containerStyles = [styles.container];
+  const bubbleStyles = [styles.bubble];
+  const dateStyles = [styles.date];
+  const imageContainerStyles = [styles.imageContainer];
+  if (false && isSelf) {
+    containerStyles.push(styles.containerSelf);
+    bubbleStyles.push(styles.bubbleSelf);
+    dateStyles.push(styles.dateSelf);
+    imageContainerStyles.push(styles.imageContainerSelf);
+  }
+
   return (
-    <div className={css(styles.container)} key={message._id}>
-      <div className={css(styles.bubble)}>
-        <span>{message.text}</span>
+    <div className={css(...containerStyles)} key={message._id}>
+      <div className={css(...bubbleStyles)}>
+        <div>{message.text}</div>
+        <div className={css(...dateStyles)}>
+          <TimeAgo date={message.createdAt} />
+        </div>
       </div>
-      <div className={css(styles.imageContainer)}>
+      {false && isSelf ?
+          <div className={css(styles.arrowSelf)}><img src="/images/green-chat-tail.png" width="6" /></div> :
+          <div className={css(styles.arrow)}><img src="/images/white-chat-tail.png" width="6" /></div>
+      }
+      <div className={css(...imageContainerStyles)}>
         <img
           className={css(styles.image)}
           src="https://avatars1.githubusercontent.com/u/2244653?v=3&s=460"
@@ -27,22 +46,55 @@ ChatMessage.propTypes = propTypes;
 const styles = StyleSheet.create({
   container: {
     position: 'relative',
-    display: 'flex',
     margin: 12,
+    display: 'flex',
+    flexDirection: 'row-reverse',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+  containerSelf: {
+    flexDirection: 'row',
   },
   bubble: {
-    border: '1px solid #ececec',
-    'border-radius': 5,
+    border: '1px solid rgb(242, 242, 242)',
+    borderRadius: 5,
     padding: 8,
-    background: '#ececec',
+    background: 'rgb(242, 242, 242)',
     width: '80%',
     display: 'inline-block',
+    flexGrow: 1,
+    borderBottomRightRadius: 5,
+    borderBottomLeftRadius: 0,
+  },
+  bubbleSelf: {
+    'background-color': 'rgb(4, 157, 145)',
+    color: 'white',
+    borderBottomRightRadius: 0,
+    borderBottomLeftRadius: 5,
+  },
+  date: {
+    'font-size': 12,
+  },
+  dateSelf: {
+    'font-weight': 'bold',
+  },
+  arrow: {
+    flexGrow: 0,
+    position: 'relative',
+    bottom: -5,
+  },
+  arrowSelf: {
+    right: 1,
   },
   imageContainer: {
     position: 'relative',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'flex-end',
+    flexGrow: 0,
+  },
+  imageContainerSelf: {
+
   },
   image: {
     width: 30,
