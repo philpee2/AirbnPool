7ff977d1eee8d035d3e85133c0a26f6107856b96
Michael Lawlor <michael.lawlor@airbnb.com>
2016-06-29 14:20:42 -0700

    Pointless comment :)

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
 import '../imports/startup/server';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  // setup Groups
 });
