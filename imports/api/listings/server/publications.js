94f5b6d6245bf0922281ce5fee5aa508d851acf3
Phil Nachum <phil.nachum@airbnb.com>
2016-06-29 12:03:52 -0700

    add listings collection

---
+++ imports/api/listings/server/publications.js
@@ -0,0 +1,5 @@
+import { Meteor } from 'meteor/meteor';
+import { Listings } from '../listings';
+
+if (Meteor.isServer) {
+}
--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -4,3 +4,5 @@ import '../../api/groups/methods';
 import '../../api/groups/server/publications';
 import '../../api/userGroups/methods';
 import '../../api/userGroups/server/publications';
+import '../../api/listings/methods';
+import '../../api/listings/server/publications';
