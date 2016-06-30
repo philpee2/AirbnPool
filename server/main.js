73eec46918b06354f8a9026264f69994ac3962e9
Warren Shen <warren.shen@airbnb.com>
2016-06-30 12:21:43 -0700

    Include url in listing seed data

--- server/main.js
+++ server/main.js
@@ -13,23 +13,25 @@ Meteor.startup(() => {
 
   Listings.insert({
     _id: '1',
-    hostImage: 'https://a0.muscache.com/im/users/2948538/profile_pic/1342408706/original.jpg?aki_policy=profile_x_medium',
+    hostImage: 'https://a1.muscache.com/im/users/177032/profile_pic/1313510327/original.jpg?aki_policy=profile_x_medium',
     location: 'Portland',
-    preview: 'https://a1.muscache.com/im/pictures/786471e1-0618-48f3-b917-c3f2fb0631d1.jpg?aki_policy=x_medium',
+    preview: 'https://a2.muscache.com/im/pictures/786471e1-0618-48f3-b917-c3f2fb0631d1.jpg?aki_policy=xx_large',
     price: '$55',
     reviews: '11',
     title: 'Spacious & Sunny Loft in the Pearl',
     type: 'Shared room',
+    url: 'https://www.airbnb.com/rooms/11674867',
   });
   Listings.insert({
     _id: '2',
-    hostImage: 'https://a2.muscache.com/im/pictures/649c9f4c-c1b5-49ac-833f-354e84abbc3a.jpg?aki_policy=profile_x_medium',
+    hostImage: 'https://a2.muscache.com/im/pictures/2c9e2e99-44b2-4b7a-9550-83bd46473b8b.jpg?aki_policy=profile_x_medium',
     location: 'Shanghai',
     preview: 'https://a2.muscache.com/im/pictures/0a35bd8f-bcc8-4a55-977a-2ed35550bd4d.jpg?aki_policy=xx_large',
     price: '$33',
     reviews: '6',
     title: 'Quiet Tatami room with garden view',
     type: 'Entire home/apt',
+    url: 'https://www.airbnb.com/rooms/12414861',
   });
   Groups.insert({ _id: '1' });
   ListingGroups.insert({ groupId: '1', listingId: '1' });
