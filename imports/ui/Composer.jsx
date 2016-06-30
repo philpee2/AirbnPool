79da69c8d67fab4538b2d6378c7777c801705ab8
Sherry Huang <sherry.huang@airbnb.com>
2016-06-30 14:42:35 -0700

    Replace input with textarea.

--- imports/ui/Composer.jsx
+++ imports/ui/Composer.jsx
@@ -50,7 +50,7 @@ class Composer extends Component {
   renderInput() {
     return (
       <form className={css(styles.form)} onSubmit={this._onSubmit}>
-        <input
+        <textarea
           ref={this._setRef}
           className={css(styles.input)}
           autoComplete="off"
@@ -121,9 +121,10 @@ const styles = StyleSheet.create({
 
   },
   input: {
-    width: '100%',
-    height: 30,
-    padding: '0 40px 0 0',
+    width: '90%',
+    height: 50,
+    resize: 'none',
+    // padding: '0 40px 0 0',
     margin: 0,
     border: 0,
     outline: 0,
